acc714923216092f5672dccf589597638f27d69e
Enjuu <30718372+JustinOsu@users.noreply.github.com>
2019-09-05 18:31:29 +0200

    Update funcmap.go

--- funcmap.go
+++ funcmap.go
@@ -17,7 +17,7 @@ import (
 	"github.com/dustin/go-humanize"
 	"github.com/gin-gonic/gin"
 	"github.com/jmoiron/sqlx"
-	"github.com/russross/blackfriday"
+	"github.com/JustinOsu/blackfriday"
 	"github.com/thehowl/qsql"
 	"golang.org/x/oauth2"
 	"zxq.co/ripple/go-discord-oauth"
